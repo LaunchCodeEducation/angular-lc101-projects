1408b89783cfc6fc91e4fda4033dbef941bca186
Jim Flores <jimflores@Jims-MacBook-Air.local>
2019-08-02 16:26:46 -0500

    Added input-practice.

---
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'movie-list',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.css']
+})
+export class MovieListComponent implements OnInit {
+  movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
