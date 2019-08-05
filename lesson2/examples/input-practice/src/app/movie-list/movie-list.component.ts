dbaeac8a23d619b941982ff0db0fd638713935d6
Jim Flores <jimflores@Jims-MacBook-Air.local>
2019-08-05 13:44:34 -0500

    Added input practice files.

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+   selector: 'movie-list',
+   templateUrl: './movie-list.component.html',
+   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-  movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
+   movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
+   duplicate = '';
 
-  constructor() { }
+   addMovie (newMovie: string) {
+      if(!this.movies.includes(newMovie)){
+         this.movies.push(newMovie);
+      } else {
+         this.duplicate = newMovie;
+      }
+      return this.duplicate;
+   }
 
-  ngOnInit() {
-  }
+   constructor() { }
 
+   ngOnInit() {
+   }
 }
