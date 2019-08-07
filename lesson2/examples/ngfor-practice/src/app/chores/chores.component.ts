b018df858ab066deb55516b4658d7e3cbdd5a8ee
Blake Mills <welzie@gmail.com>
2019-08-07 13:42:05 -0500

    using array of objects for chores

--- lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores"];
+   chores = [
+      {title: "Yesterday's Chores", chores: ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries']},
+      {title: "Today's Chores", chores: ['Schedule Doc Visit', 'Go to class!']},
+      {title: "Tomorrow's Chores", chores: ['Sweep', 'Organize closet', 'Prep work for next class']}
+   ];
 
    constructor() { }
 
