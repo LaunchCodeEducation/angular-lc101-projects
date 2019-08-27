04496459141ee4f728e6bf93542c549619f15a2b
Jim Flores <jimflores@Jims-MacBook-Air.local>
2019-08-27 11:49:20 -0500

    Updated skill-set.component.ts to match master.

--- lesson3/examples/src/app/skill-set/skill-set.component.ts
+++ lesson3/examples/src/app/skill-set/skill-set.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'skill-set',
-  templateUrl: './skill-set.component.html',
-  styleUrls: ['./skill-set.component.css']
+   selector: 'skill-set',
+   templateUrl: './skill-set.component.html',
+   styleUrls: ['./skill-set.component.css']
 })
 export class SkillSetComponent implements OnInit {
+   listHeading: string = 'Some Coding Skills I Know';
+   skills: string[] = ['Loops', 'Conditionals', 'Functions', 'Classes', 'Modules', 'Git', 'HTML/CSS'];
+   textColors: string[] = ['black', 'violet', 'lightblue'];
 
-  constructor() { }
+   constructor() { }
 
-  ngOnInit() {
-  }
+   ngOnInit() {
+   }
 
 }
