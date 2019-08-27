96b6997a14cea488f96acedcc2596043036b9181
Jim Flores <jimflores@Jims-MacBook-Air.local>
2019-08-27 11:47:51 -0500

    Added skill-set component.

--- lesson3/examples/src/app/app.module.ts
+++ lesson3/examples/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillSetComponent
   ],
   imports: [
     BrowserModule
---
+++ lesson3/examples/src/app/skill-set/skill-set.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'skill-set',
+  templateUrl: './skill-set.component.html',
+  styleUrls: ['./skill-set.component.css']
+})
+export class SkillSetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
