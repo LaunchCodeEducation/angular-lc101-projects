d0530d0c10ea455abf8a0eec86ff19377986afbd
Carly Langlois <cj.langlois.cl@gmail.com>
2019-09-02 18:23:22 -0500

    experiement with slightly more complicated logic, arrays

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -7,11 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Exercises: Angular Lesson 3';
-  flightStatus = "Space shuttle ready for takeoff!";
+  statusMessages = ['Space shuttle ready for takeoff!', 'Shuttle in flight'];
+  colors = ['green', 'blue'];
+  
+  flightStatus = this.statusMessages[0];
   isLanded = true;
   spaceShuttleHeight = 0;
+  shuttleBackground = this.colors[0];
 
   toggleMissionStatus() {
+    confirm('message??');
     this.isLanded = !this.isLanded;
+    this.shuttleBackground = this.shuttleBackground === this.colors[0] ? this.colors[1] : this.colors[0];
+    this.flightStatus = this.flightStatus === this.statusMessages[0] ? this.statusMessages[1] : this.statusMessages[0];
   }
 }
