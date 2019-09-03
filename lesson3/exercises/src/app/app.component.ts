ea749a6f658828206fb0b76b23c9bf7d76a6bd64
Carly Langlois <cj.langlois.cl@gmail.com>
2019-09-02 22:51:08 -0500

    solution matching dom and event chapter solution

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -15,10 +15,72 @@ export class AppComponent {
   spaceShuttleHeight = 0;
   shuttleBackground = this.colors[0];
 
-  toggleMissionStatus() {
-    confirm('message??');
-    this.isLanded = !this.isLanded;
-    this.shuttleBackground = this.shuttleBackground === this.colors[0] ? this.colors[1] : this.colors[0];
-    this.flightStatus = this.flightStatus === this.statusMessages[0] ? this.statusMessages[1] : this.statusMessages[0];
+  initStatus = {
+    color: 'green',
+    height: 0,
+    message: 'Space shuttle ready for takeoff!',
+  };
+
+  shuttleTakeOff = {
+    color: 'blue',
+    height: 10000,
+    message: 'Shuttle in flight.',
+  };
+
+  shuttleLand = {
+    color: 'green',
+    height: 0,
+    message: 'Shuttle landed.',
+  };
+
+  shuttleMissionAbort = {
+    color: 'green',
+    height: 0,
+    message: 'Mission aborted.',
+  };
+
+  status = this.initStatus;
+
+  handleRightClick(img) {
+    let movement = parseInt(img.style.left) + 10 + 'px';
+    img.style.left = movement;
+  }
+
+  handleLeftClick(img) {
+    let movement = parseInt(img.style.left) - 10 + 'px';
+    img.style.left = movement;
+  }
+
+  handleDownClick(img) {
+    let movement = parseInt(img.style.bottom) - 10 + 'px';
+    img.style.bottom = movement;
+    this.status.height = this.status.height - 10000;
+  }
+
+  handleUpClick(img) {
+    let movement = parseInt(img.style.bottom) + 10 + 'px';
+    img.style.bottom = movement;
+    this.status.height = this.status.height + 10000;
+  }
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+      this.status = this.shuttleTakeOff;
+    }
+  }
+
+  handleLand(img) {
+    window.alert('The shuttle is landing. Landing gear engaged.');
+    this.status = this.shuttleLand;
+    img.style.bottom = '0px';
+  }
+
+  handleMissionAbort(img) {
+    let result = window.confirm('Are you sure you want to end the mission?');
+    if (result) {
+      this.status = this.shuttleMissionAbort;
+      img.style.bottom = '0px';
+    }
   }
 }
