4697f1789a2eff8c677e84bbe1564605ecf347b0
Carly Langlois <cj.langlois.cl@gmail.com>
2019-09-03 13:53:22 -0500

    add only lesson3 exercises!

---
+++ lesson3/exercises/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Exercises: Angular Lesson 3';
+
+  initStatus = {
+    color: 'green',
+    height: 0,
+    message: 'Space shuttle ready for takeoff!',
+  };
+
+  shuttleTakeOff = {
+    color: 'blue',
+    height: 10000,
+    message: 'Shuttle in flight.',
+  };
+
+  shuttleLand = {
+    color: 'green',
+    height: 0,
+    message: 'Shuttle landed.',
+  };
+
+  shuttleMissionAbort = {
+    color: 'green',
+    height: 0,
+    message: 'Mission aborted.',
+  };
+
+  status = this.initStatus;
+}
