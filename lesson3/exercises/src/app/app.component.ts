5529ab995ac3ea156977d57bf73bb6721875fd8c
Carly Langlois <cj.langlois.cl@gmail.com>
2019-09-08 22:54:40 -0500

    update starter code to not use status objs

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -8,29 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Exercises: Angular Lesson 3';
 
-  initStatus = {
-    color: 'green',
-    height: 0,
-    message: 'Space shuttle ready for takeoff!',
-  };
-
-  shuttleTakeOff = {
-    color: 'blue',
-    height: 10000,
-    message: 'Shuttle in flight.',
-  };
-
-  shuttleLand = {
-    color: 'green',
-    height: 0,
-    message: 'Shuttle landed.',
-  };
-
-  shuttleMissionAbort = {
-    color: 'red',
-    height: 0,
-    message: 'Mission aborted.',
-  };
-
-  status = this.initStatus;
+  color = 'green';
+  height = 0;
+  width = 0;
+  message = 'Space shuttle ready for takeoff!';
 }
