47596cb7a5d62e4a39b0b664d5063a582adefcf8
AlphieKiu <kiuhuaien87@gmail.com>
2019-10-22 21:35:56 -0500

    finished part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://www.humanesociety.org/sites/default/files/styles/1441x612/public/2018/08/kitten-440379.jpg?h=f6a7b1af&itok=HVqvfhtg';
+  image3 = 'https://www.sciencenewsforstudents.org/sites/default/files/scald-image/860_cat_names.png';
 
   constructor() { }
 
