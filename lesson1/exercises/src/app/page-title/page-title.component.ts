06c3a0fe844273b00a1b754f383cb1a3149207be
ianmottert <motterti@gmail>
2021-08-07 11:19:43 -0500

    lesson1 exercises finished

---
+++ lesson1/exercises/src/app/page-title/page-title.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'page-title',
+  templateUrl: './page-title.component.html',
+  styleUrls: ['./page-title.component.css']
+})
+export class PageTitleComponent implements OnInit {
+
+  title: string = 'Angular Lesson1 Exercises';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
