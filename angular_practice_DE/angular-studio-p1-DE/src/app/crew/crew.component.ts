c6c9354e2b3fb8ebf0c2d67032a2df1faa55f91f
David <emmertd30@gmail.com>
2022-12-19 12:09:33 -0500

    initial commit

---
+++ angular_practice_DE/angular-studio-p1-DE/src/app/crew/crew.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crew',
+  templateUrl: './crew.component.html',
+  styleUrls: ['./crew.component.css']
+})
+export class CrewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular_practice_DE/angular-studio-p1-DE/src/app/experiments/experiments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experiments',
+  templateUrl: './experiments.component.html',
+  styleUrls: ['./experiments.component.css']
+})
+export class ExperimentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
